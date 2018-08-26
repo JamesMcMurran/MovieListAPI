78ecc198c0c0dec032c7a5646471070f08c9778a
james <james@mcmurran.org>
2018-08-25 20:39:33 -0400

    Removing the echo of the db settings

--- index.php
+++ index.php
@@ -3,9 +3,4 @@
 //Since this is not a docker and is using forge.laravel.com the env are stored in this file
 include '.env';
 
-echo 'CI/CD environment works';
-
-echo '< /br>';
-
-var_dump([ getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASSWORD'), getenv('DB_TABLE')]);
-
+echo 'It works';
