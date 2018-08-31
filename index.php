df0fb78fd22c95e137c5d6ef0fbd6a8f50f424ad
james <james@mcmurran.org>
2018-08-30 21:39:54 -0400

    Added message quick message for insert

--- index.php
+++ index.php
@@ -6,4 +6,9 @@ include 'Models/Movies.php';
 
 $movies = new \movieList\Movies();
 
-$movies->create("TEST movie".rand(0,999999) ,"VHS",rand(1,500),rand(1800,2100),rand(1,5));
+try{
+	$movies->create("TEST movie".rand(0,999999) ,"VHS",rand(1,500),rand(1800,2100),rand(1,5));
+	echo 'Insert completed';
+} catch (\Exception $e) {
+	echo 'Caught exception: ',   $e->getMessage(), "\n";
+}
